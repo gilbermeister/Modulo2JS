34249d8d199ea3682ed7f66434a51d3a7cabd20e
gilbermeister <gilbertocervantes@outlook.com>
2022-01-24 21:28:11 -0600

    Intro a JS

---
+++ 02-introAJS/01-intro/js/index.js
@@ -0,0 +1,17 @@
+//Elementos para mostrar la salida de datos
+
+//Muestra la salida en la consola
+console.log('Hola Mundo, desde archivo externo')
+console.log(5+10)
+console.log(4>3)
+console.log('3'===3)
+
+//Muestra la salida en la página
+document.write('Hola mundo, desde un archivo externo')
+document.write(4>3)
+document.write('3'===3)
+
+//Muestra un popup con la salida de datos
+alert('Hola mundo, desde un archivo externo')
+alert(4>3)
+alert('3'===3)
