b23fc1d2bd6f3926c6844a1ca5427777ef9fd6d1
gilbermeister <gilbertocervantes@outlook.com>
2022-02-08 18:46:18 -0600

    hasta ejercisio de edad y cambio de color

--- 02-introAJS/09-domJs/js/index.js
+++ 02-introAJS/09-domJs/js/index.js
@@ -1,4 +1,4 @@
-let txtNombre, añoNac, edad, añoActual;
+let txtNombre, añoNac, edad, añoActual, mesNac, mesActual, meses;
 
 function capturaNombre(){
     txtNombre = document.getElementById('nombre');
@@ -9,13 +9,34 @@ function calcularEdad(){
     let fecha = new Date();
     añoNac = Number(document.getElementById("fecha").value.slice(0,4));
     añoActual = fecha.getFullYear();
-    if(añoActual<añoNac){
+    mesNac = Number(document.getElementById("fecha").value.slice(5,7));
+    //console.log(document.getElementById("fecha").value);
+    mesActual = fecha.getMonth()+1;
+    //console.log(fecha.getMonth());
+    if(añoActual < añoNac){
         alert('Elige un año de nacimiento valido');
+    }else if(mesNac>mesActual){
+        edad = añoActual - añoNac -1;
+        meses = 12 - 4 + mesActual;
+        document.getElementById("edad").innerText = 'Tu edad es ' + edad + ' con ' + meses + ' meses';
     }else{
+        
         edad = añoActual - añoNac;
-        alert('Su edad es ' + edad + ' años'); 
+        meses = mesActual - mesNac;
+        document.getElementById("edad").innerText = 'Tu edad es ' + edad + ' con ' + meses + ' meses';
     }
     
 }
 
 document.getElementById("calcular").addEventListener('click', calcularEdad);
+
+let control = 0;
+function cambiarColor(){
+    if(control == 0){
+    document.getElementById("cuerpo").style.backgroundColor = "#b2dfdb";
+    control = 1;
+}else{
+    document.getElementById("cuerpo").style.backgroundColor = "#90caf9";
+    control = 0;
+}
+}
