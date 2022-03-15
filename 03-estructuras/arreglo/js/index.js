95f5c85b09a9750b50b857ff7341a14073b482a0
gilbermeister <gilbertocervantes@outlook.com>
2022-03-14 19:28:26 -0600

    ejercico random

---
+++ 03-estructuras/arreglo/js/index.js
@@ -0,0 +1,19 @@
+'use strict'
+const arreglo = [
+    {
+        nombre: 'alan',
+        lugarNacimiento: {
+            pais: 'mexico',
+            ciudad: ''
+        }
+    },
+    {
+        nombre: 'enrique'
+    },
+    {
+        nombre: 'ariadne'
+    },
+    {
+        nombre: 'beatriz'
+    }
+];
